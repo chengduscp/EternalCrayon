3e85fdca9da02dff7aa8f5108048f47c2f939248
CS118 <cs118@ubuntu.(none)>
2013-10-12 13:42:04 -0700

    Updated Webserver

--- webserver.c
+++ webserver.c
@@ -18,7 +18,9 @@ int main(int argc, char *argv[])
 {
    int sockfd, newsockfd, portno, pid;
    struct sockaddr_in server, client;
-   
+   socklen_t clientLen;   
+
+
    if(argc < 2)
    {
       fprintf(stderr, "ERROR, no port provided\n");
@@ -34,6 +36,39 @@ int main(int argc, char *argv[])
    server.sin_port=htons(portno);
 
 
+   if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0)
+      error("BIND ERROR");
+
+   listen(sockfd,5);
+
+   newsockfd = accept(sockfd, (struct sockaddr *) &client, &clientLen);
+
+   if(newsockfd < 0)
+   {
+      error("ERROR ON ACCEPT");
+   }
+
+   int n;
+   char buffer[256];
+   
+   n = read(newsockfd, buffer, 255);
+   if(n < 0)
+   {
+     error("Error reading from socket");
+   }
+
+   printf("Here is the message: %s\n", buffer);
+
+
+   n = write(newsockfd, "I got your message", 18);
+
+   if(n < 0)
+     error("Error writing to socket");
+
+ 
+   close(newsockfd);
    close(sockfd);
+
+   return 0;
 
 }
